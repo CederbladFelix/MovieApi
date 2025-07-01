1c072cb7855968e999a7dbbe84cf0e7230dc8480
Felix Cederblad <felix_cederblad@hotmail.com>
2025-07-01 10:04:05 +0200

    Add project files.

---
+++ MovieApi/Models/Entities/MovieDetails.cs
@@ -0,0 +1,13 @@
+﻿namespace MovieApi.Models.Entities
+{
+    public class MovieDetails
+    {
+        public int Id { get; set; }
+        public string Synopsis { get; set; } = null!;
+        public string Language { get; set; } = null!;
+        public int Budget { get; set; }
+
+        public Movie Movie { get; set; } = null!;
+        public int MovieId { get; set; }
+    }
+}
---
+++ MovieApi/Program.cs
@@ -0,0 +1,39 @@
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using MovieApi.Data;
+
+namespace MovieApi
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+            builder.Services.AddDbContext<MovieApiContext>(options =>
+                options.UseSqlServer(builder.Configuration.GetConnectionString("MovieApiContext") ?? throw new InvalidOperationException("Connection string 'MovieApiContext' not found.")));
+
+            // Add services to the container.
+
+            builder.Services.AddControllers();
+            // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
+            builder.Services.AddOpenApi();
+
+            var app = builder.Build();
+
+            // Configure the HTTP request pipeline.
+            if (app.Environment.IsDevelopment())
+            {
+                app.MapOpenApi();
+            }
+
+            app.UseHttpsRedirection();
+
+            app.UseAuthorization();
+
+
+            app.MapControllers();
+
+            app.Run();
+        }
+    }
+}
