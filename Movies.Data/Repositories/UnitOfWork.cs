7f6694066618dad30ec4420b02a17c20da78f5d0
Felix Cederblad <felix_cederblad@hotmail.com>
2025-07-10 16:54:59 +0200

    Readonly uow

--- Movies.Data/Repositories/UnitOfWork.cs
+++ Movies.Data/Repositories/UnitOfWork.cs
@@ -6,9 +6,9 @@ namespace Movies.Data.Repositories
     public class UnitOfWork : IUnitOfWork
     {
         private readonly MovieApiContext _movieApiContext;
-        private Lazy<IMovieRepository> _movieRepository;
-        private Lazy<IActorRepository> _actorRepository;
-        private Lazy<IReviewRepository> _reviewRepository;
+        private readonly Lazy<IMovieRepository> _movieRepository;
+        private readonly Lazy<IActorRepository> _actorRepository;
+        private readonly Lazy<IReviewRepository> _reviewRepository;
         public IMovieRepository Movies => _movieRepository.Value;
         public IActorRepository Actors => _actorRepository.Value;
         public IReviewRepository Reviews => _reviewRepository.Value;
@@ -18,7 +18,7 @@ namespace Movies.Data.Repositories
             _movieRepository = new Lazy<IMovieRepository>(() => new MovieRepository(movieApiContext));
             _actorRepository = new Lazy<IActorRepository>(() => new ActorRepository(movieApiContext));
             _reviewRepository = new Lazy<IReviewRepository>(() => new ReviewRepository(movieApiContext));
-            this._movieApiContext = movieApiContext;
+            _movieApiContext = movieApiContext;
         }
 
         public async Task CompleteAsync() => await _movieApiContext.SaveChangesAsync();
