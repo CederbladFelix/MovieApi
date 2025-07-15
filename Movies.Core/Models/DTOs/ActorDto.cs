e9c7a9c0243b99861769c296b46ce3a4727bc7d8
Felix Cederblad <felix_cederblad@hotmail.com>
2025-07-15 17:29:38 +0200

    Remove validation for non creat/put dtos

--- Movies.Core/Models/DTOs/ActorDto.cs
+++ Movies.Core/Models/DTOs/ActorDto.cs
@@ -5,11 +5,7 @@ namespace Movies.Core.Models.DTOs
     public class ActorDto
     {
         public int Id { get; set; }
-
-        [Required]
-        [MaxLength(100)]
         public string Name { get; set; } = null!;
-
         public int BirthYear { get; set; }
     }
 
