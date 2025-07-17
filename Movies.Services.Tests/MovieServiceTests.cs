1a13eaeda0139521bdd0654a2f394b26ac0772e0
Felix Cederblad <felix_cederblad@hotmail.com>
2025-07-17 16:39:13 +0200

    Test Project

---
+++ Movies.Services.Tests/MovieServiceTests.cs
@@ -0,0 +1,82 @@
+﻿using AutoMapper;
+using Bogus;
+using Microsoft.Extensions.Logging;
+using Moq;
+using Movies.Core.DomainContracts;
+using Movies.Core.Models.DTOs;
+using Movies.Core.Models.Entities;
+using Movies.Data.Data;
+using System.Globalization;
+
+namespace Movies.Services.Tests 
+{
+    public class MovieServiceTests
+    {
+        private Mock<IUnitOfWork> _uowMock;
+        private MovieService sut;
+        private Mapper _mapper = new Mapper(new MapperConfiguration(cfg => cfg.AddProfile<MapperProfile>(), new LoggerFactory()));
+
+        public MovieServiceTests()
+        {
+            _uowMock = new Mock<IUnitOfWork>();
+
+            sut = new MovieService(_uowMock.Object, _mapper);
+        }
+
+        [Fact]
+        public async Task GetMoviesAsync_Returns_Expected_PagedResult()
+        {
+            //Arrange
+            const int expectedMovies = 5;
+            _uowMock.Setup(x => x.Movies.MovieCountAsync()).ReturnsAsync(10);
+            _uowMock.Setup(x => x.Movies.GetMoviesAsync(It.IsAny<PaginationOptionsDto>(), true)).ReturnsAsync(GenerateMovies(expectedMovies));
+
+            //Act
+            var resultDto = await sut.GetMoviesAsync(new PaginationOptionsDto { CurrentPage = 1, PageSize = 10 }, true);
+
+            //Assert
+            Assert.IsType<PagedResultDto<MovieDto>>(resultDto);
+            Assert.Equal(expectedMovies, resultDto.Data.Count());
+        }
+        
+        public List<Movie> GenerateMovies(int numberOfMovies)
+        {
+            var movies = new List<Movie>();
+            var faker = new Faker();
+            var genreNames = new[]
+{
+                "Action",
+                "Drama",
+                "Comedy",
+                "Horror",
+                "Documentary",
+                "Fantasy"
+            };
+
+            string[] languages = new[] { "English", "Swedish", "French", "German", "Japanese" };
+
+            for (int i = 1; i <= numberOfMovies; i++)
+            {
+                var movie = new Movie
+                {
+                    Title = CultureInfo.CurrentCulture.TextInfo.ToTitleCase(faker.Commerce.ProductName()),
+                    Year = faker.Date.Past(20).Year,
+                    Duration = faker.Random.Int(60, 180),
+                    GenreId = faker.Random.Int(1, 10),
+                    Genre = new Genre{ Name = faker.PickRandom(genreNames) },
+                    MovieDetails = new MovieDetails
+                    {
+                        Synopsis = faker.Lorem.Sentence(20),
+                        Language = faker.PickRandom(languages),
+                        Budget = faker.Random.Int(20000, 1000000)
+                    }
+                };
+
+                movies.Add(movie);
+            }
+
+            return movies;
+        }
+
+    }
+}
